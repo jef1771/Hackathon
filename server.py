c49d6429083804c11ad677b82e9415104ca4f31f
Steven <4smalmgren@gmail.com>
2018-04-29 14:49:35 -0400

    Update for requirements

--- server.py
+++ server.py
@@ -1,43 +1,60 @@
-from flask import Flask, request, send_from_directory, Response, redirect, url_for, g
+import os
+from flask_login import login_required, login_url, UserMixin, LoginManager, login_user, current_user
+
+from flask import Flask, request, send_from_directory, Response, redirect, url_for, g, flash
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
 from functools import wraps
-
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = "Whoop$"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 database = os.getcwd() + '/database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-engine = create_engine('sqlite:///'+database)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+engine = create_engine('sqlite:///' + database)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
-# """
-#    ADD ROUTES HERE FOR SPECIFIC PAGES
-# """
+#
+#  LOGIN MANAGEMENT
+#
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#
+#  END LOGIN MANAGEMENT
+#
+
+
+
+# """
+#    ROUTES
+# """
 
 @app.route('/')
 def splash():
     return send_from_directory('static/html', "splash.html")
 
 
-@app.route('/login')
-def login():
+@app.route('/login', methods=['GET'])
+def login_get():
     return send_from_directory('static/html', "login.html")
 
 
 @app.route('/home')
+@login_required
 def home():
     return send_from_directory('static/html', "home.html")
 
@@ -94,9 +111,18 @@ def login_post():
     # here and if they have the correct credentials, return a
     # token to put in the cookies. If they don't exist send that
     # as the response
+    #
+    # Opted to use flask login instead, handles a lot for us
+    # use the current_user object
     username = request.form["user"]
     password = request.form["pass"]
-    return send_from_directory('static/html', "home.html")
+
+    user = User.query.filter(User.username == username).first()
+    if user is None or not user.check_password(password):
+        flash('Invalid Login')
+        return Response('Invalid Login')
+    login_user(user)
+    return Response('Nice, those are valid credentials!')
 
 
 @app.route('/signup', methods=['POST'])
@@ -105,8 +131,9 @@ def signup():
         username = request.form["user"]
         password = request.form["pass"]
         if is_unique_name(username):
-            create_new_user(username, password)
-            return "u all signed in!"
+            new_user = create_new_user(username, password)
+            login_user(new_user)
+            return Response("Nice new login!")
         else:
             return Response("that shit taken")
     except KeyError:
@@ -115,8 +142,8 @@ def signup():
     except BaseException as err:
         print(err.args[0])
 
-
-    return Response("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!")
+    return Response("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our "
+                    "headquarters are working VEWY HAWD to fix this!")
 
 
 # """
@@ -127,14 +154,19 @@ def signup():
 # DB Objects
 # """
 
-
-class User(Base):
+# this is both flasks login object and the database user model
+class User(Base, UserMixin):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=True)
     skills = Column(String(1000), unique=False, nullable=True)
     password = Column(String(20), unique=False, nullable=False)
+
+    def check_password(self, password):
+        user = User.query.filter_by(id=self.id).first()
+        return password == user.password
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -170,6 +202,9 @@ class Contributor(Base):
 # DB Functions
 # """
 
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -177,7 +212,6 @@ def shutdown_session(exception=None):
 
 
 def get_users_projects(username):
-
     return
 
 
@@ -195,8 +229,11 @@ def create_new_user(username, password):
         new_user = User(username=username, password=password, skills="I like to party!")
         db_session.add(new_user)
         db_session.commit()
+        return new_user
     except BaseException as err:
         print(err.args[0])
+        flash('Error creating new user.')
+        return redirect(url_for('.login'))
 
 
 def requires_auth(f):
@@ -211,7 +248,7 @@ def requires_auth(f):
 
 
 def authenticate():
-    #"""Sends a 401 response that enables basic auth"""
+    # """Sends a 401 response that enables basic auth"""
     return Response(
         'Could not verify your access level for that URL.\n'
         'You have to login with proper credentials', 401,
@@ -226,4 +263,3 @@ def check_auth(username, password):
 if __name__ == '__main__':
     init_db()
     app.run()
-
